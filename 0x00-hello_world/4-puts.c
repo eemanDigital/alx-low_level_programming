effcea54a5d1acbe273e89398bb9bf8b15d3385d
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-08 15:41:58 -0700

    Task 5 completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point function
+ *
+ * Description: using main function for output
+ * the programe is meant to "Print strings"
+ * It return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
