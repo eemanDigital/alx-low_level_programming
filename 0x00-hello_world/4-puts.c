10784e8c55e3b70da5515b30cf2f102b7013a34e
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-08 15:51:35 -0700

    Done with taks 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - entry point function
+ * main - Print string in the function
  *
- * Description: using main function for output
- * the programe is meant to "Print strings"
- * It return 0
+ * Description: using main function
+ * the programe output "Programming is like building a multilingual puzzle"
+ *  Return: 0
  */
 int main(void)
 {
