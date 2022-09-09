bb7518499f85589b5071a2d74eafb2f91f2807a3
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-09 16:12:43 -0700

    Task advance 2 completed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - my entry point
+ *
+ * Description: code to print combination
+ * of single digit
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int i;
+	int a;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	for (i = '0'; i <= '9'; i++)
+	{
+	for (a = '0'; a <= '9'; a++)
+	{
+	if (c < i)
+	{
+		putchar(c);
+		putchar(i);
+		putchar(a);
+	if (c != '7')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
