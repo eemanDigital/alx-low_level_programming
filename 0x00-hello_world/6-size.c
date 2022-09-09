2a156e3e8322826608da8954870fbdc747def28a
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-08 17:10:27 -0700

    Hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("size of char:%1d byte(s)\n", sizeof(c));
-	printf("size of int:%1d byte(s)\n", sizeof(i));
-	printf("size of li:%1d byte(s)\n", sizeof(li))
-	printf("size of lli:%1d byte(s)\n", sizeof(lli));
-	printf("size of float:%1d byte(s)\n", sizeof(f));
+	printf("size of char: %1d byte(s)\n", sizeof(c));
+	printf("size of int: %1d byte(s)\n", sizeof(i));
+	printf("size of li: %1d byte(s)\n", sizeof(li))
+	printf("size of lli: %1d byte(s)\n", sizeof(lli));
+	printf("size of float: %1d byte(s)\n", sizeof(f));
 	return (0);
 }
