44aecf944b7b3112507d512c7e191e3aad127617
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-13 10:35:52 -0700

    Task 6 completed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *int_abs - entry point
+ * Description: Print absolute value of integer
+ *
+ * @n: check function input
+ *
+ * Return: returns outputs
+ */
+int int_abs(int n)
+{
+	if (n > 0)
+	{
+	return (n);
+	}
+	else if (n < 0)
+	{
+	return (-n);
+	}
+	return (n);
+
+}
