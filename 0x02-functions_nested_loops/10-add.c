b158f7887d8b974308a46f3da1341334ab454f13
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-13 16:09:10 -0700

    Task 10 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - sum to integers
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: the addition of numbers
+ */
+int add(int x, int y)
+{
+	int total;
+
+	total = x + y;
+	return (total);
+}
