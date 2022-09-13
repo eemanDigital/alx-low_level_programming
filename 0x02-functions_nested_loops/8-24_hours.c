9c4090e78e061add750a40944f9a98de37d2a053
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-13 14:26:26 -0700

    Done with task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *jack_bauer - print every minute of the day of Jack Baue
+ */
+void jack_bauer(void)
+{
+	int minutes;
+	int hours;
+
+	for (hours = 0; minutes <= 23; ++hours)
+	{
+	for (minutes = 0; minutes <= 59; ++minutes)
+	{
+	_putchar((hours / 10) + 48);
+	_putchar((hours % 10) + 48);
+	_putchar(':');
+	_putchar((minutes / 10) + 48);
+	_putchar((hours % 10) + 48);
+	_putchar('\n');
+	}
+	}
+}
