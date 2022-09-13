b22add87dc93ad04cf749f1e48a95d0ab3ab6b59
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-13 06:56:39 -0700

    Task 1 completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - entru point func
+ *
+ * Description: prints the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	_putchar(c);
+	_putchar('\n');
+}
+
