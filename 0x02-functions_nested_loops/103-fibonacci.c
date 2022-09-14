95d30636dfe450ceff4db09ec05bbe1bc8f236c2
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-14 01:33:29 -0700

    Task 14 completed

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: programe to  print
+ * program that finds and prints
+ * the sum of the even-valued terms
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long num1 = 0, num2 = 1, sum;
+	float total;
+
+	while (1)
+	{
+	sum = num1 + num2;
+
+	if (sum > 4000000)
+	break;
+	if ((sum % 2) == 0)
+	total += sum;
+	num1 = num2;
+	num2 = num1;
+
+	}
+	printf("%.0f\n", total);
+	return (0);
+}
