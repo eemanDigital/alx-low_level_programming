4a28ec721c76f497fbdaf2001b0bc62ff3aace56
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-15 09:00:48 -0700

    Task 9 completed

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * main - entry function
+ * Description: print number and fizzbuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1;
+
+	while (a <= 100)
+	{
+	if (a % 3 == 0 && a & 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (a % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (a % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%i", a);
+	}
+	if (a != 100)
+	{
+	putchar(' ');
+	}
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
