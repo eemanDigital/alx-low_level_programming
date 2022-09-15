a07e322da9ebddfb8d29c52c417045820f1a9bbc
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-15 10:04:03 -0700

    Task 12 completed

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - entry function
+ * @n: var
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+	_putchar(45);
+	i = -n;
+	}
+	if (i / 10)
+	print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
