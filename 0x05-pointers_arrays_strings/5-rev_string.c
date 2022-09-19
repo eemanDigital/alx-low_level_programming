dc938ec7d1e9bb22ef65e9f974f7fbcede78982d
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-19 09:25:57 -0700

    Tast 5 completed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - function to reverse a string
+ * @s: string parameter input
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length, a;
+	char ch;
+
+	for (length = 0; s[length] != \'0'; ++length)
+	;
+	for (a = 0; a < length / 2; ++a)
+	{
+	ch = s[a];
+	s[a] = s[length - 1 - a];
+	s[length - 1 - a] = ch;
+	}
+}
