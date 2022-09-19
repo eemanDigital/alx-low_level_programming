a853a86842df3f7f81d4425ae699b92536a3e901
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-19 11:08:43 -0700

    Task 101 Advance completed

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - function to generates random password
+ *  Return: 0
+ */
+int main(void)
+{
+	int s;
+	char ch;
+
+	srand(time(NULL));
+	while (s <= 26450)
+	{
+	c = rand() % 128;
+	sum += ch;
+	putchar(ch);
+	}
+	putchar(2772 - s);
+	return (0);
+}
