7ac028fcf304aa93d181e91176fc95bf265d5201
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-19 07:43:06 -0700

    Task 3 completed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: point variable
+ * Return: the legnth of the string
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	if (s[l])
+	l++;
+	return (l);
+}
