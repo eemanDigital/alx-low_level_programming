7ac552c9987bef09a4aa711171f093460b0c9126
Lukman Asinmi <asinmilukman@gmail.com>
2022-09-30 10:20:58 -0700

    Task 0 Completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- a program that prints its name, followed by a new line.
+ * @argc: store the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
